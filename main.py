ca72a5e39685e2b5e70861fe0b8907daa4f32ca0
Ethan Chapman <ethan.chapman0@outlook.com>
2024-04-09 08:01:18 -0600

    bruv

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ async def post_msg(req: NewMsg):
     except:
         raise HTTPException(status_code=404)
     m_id = uuid.uuid4()
-    messages[m_id] = m
+    messages[m_id] = m.data
     return MsgResponse(messageId=m_id, encryptedMessage=m.data)
 
 
@@ -195,4 +195,4 @@ async def get_msg(messageId: UUID4):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=9851, log_level="info")
